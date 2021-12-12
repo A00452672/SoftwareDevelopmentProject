dc33d162d41ef73987328f2fb91e4f31ad8d6cb1
Arun Kumar Vasudevan <arun.vasudevan@smu.ca>
2021-12-11 21:15:03 -0400

    Initial commit

---
+++ Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SoftwareDevelopmentProject.Models;
+
+namespace SoftwareDevelopmentProject.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Registration(int id=0)
+        {
+            User userModel = new User();
+            return View(userModel);
+        }
+    }
+}
